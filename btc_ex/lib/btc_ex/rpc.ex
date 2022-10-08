580948d95eb94bd9445700eb3dbf86d70a454919
jrigada <jrigada@frba.utn.edu.ar>
2022-10-08 18:05:43 -0500

    first bitcoin endpoints

---
+++ btc_ex/lib/btc_ex/rpc.ex
@@ -0,0 +1,54 @@
+defmodule BtcEx.RPC do
+  @client Tesla.client([
+    {Tesla.Middleware.BasicAuth, username: "asd", password: "asd"},
+    {Tesla.Middleware.Headers, [{"content-type", "text/plain"}]}
+  ])
+
+  def get_mempool_info() do
+    send_request("getmempoolinfo", [])
+  end
+
+  def get_raw_mempool(verbose) do
+    send_request("getrawmempool", [verbose])
+  end
+
+  def list_unspent() do
+    send_request("listunspent", [])
+  end
+
+  def generate_raw_transaction(txid, vout, address, amount) do
+    input = %{
+      "txid" => txid,
+      "vout" => vout,
+    }
+    output = %{
+      "#{address}" => amount
+    }
+    send_request("createrawtransaction", [[input], [output]])
+  end
+  
+  def sign_raw_transaction(message) do
+    send_request("signmessage", ["bcrt1q5cc0xvj38546a2zeggph5d3ewnpqjpe9u7q3d8", message])
+  end
+
+  def get_tx_out(txid, vout) do
+    send_request("gettxout", [txid, vout])
+  end
+
+  defp send_request(method, args) do
+    payload = build_payload(method, args)
+
+    {:ok, response} = Tesla.post(@client, "http://127.0.0.1:18443/", payload)
+    response.body |> Jason.decode!()
+  end
+
+  defp build_payload(method, params) do
+    %{
+      jsonrpc: "1.0",
+      id: Enum.random(1..9_999_999),
+      method: method,
+      params: params
+    }
+    |> Jason.encode!()
+  end
+end
